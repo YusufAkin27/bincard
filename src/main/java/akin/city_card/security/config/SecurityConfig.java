3429c56bee731eb35c1d0cfced8dcfe4c2ed1b46
Yusuf Akin <136736997+YusufAkin27@users.noreply.github.com>
2025-06-24 01:23:27 +0300

    haberler düzenlendi

--- src/main/java/akin/city_card/security/config/SecurityConfig.java
+++ src/main/java/akin/city_card/security/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                 "/v1/api/user/verify/email/send",
                 "/v1/api/user/verify/phone/resend/**",// email doğrulama linki gönderme
                 "/v1/api/user/password/forgot/**",    // şifre sıfırlama kodu gönderme
-                "/v1/api/user/password/reset/**",     // şifre sıfırlama
+                "/v1/api/user/password/reset/**",
+                "/v1/api/admin/sign-up/**",// şifre sıfırlama
                 "/v1/api/user/active/**",              // aktif etme işlemleri varsa
                 // Diğer izin verilenler (örnek: token yenileme)
                 "/v1/api/token/**",
