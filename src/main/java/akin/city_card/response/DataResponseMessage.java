175b738659c51a3c941dcd64d5752bd7e75ca5d6
YusufAkin27 <ysufakin23@gmail.com>
2025-07-04 16:21:29 +0300

    hata düzenlendi

--- src/main/java/akin/city_card/response/DataResponseMessage.java
+++ src/main/java/akin/city_card/response/DataResponseMessage.java
@@ -4,8 +4,7 @@ package akin.city_card.response;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder
-@Data
+
 public class DataResponseMessage<T> extends ResponseMessage {
     private T data;
 
