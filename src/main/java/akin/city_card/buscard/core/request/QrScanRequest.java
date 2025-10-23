570e8f763d1701dc493b95f6957704ca3b70ad85
ysufa <ysufakin23@gmail.com>
2025-10-23 18:54:58 +0300

    qr

--- src/main/java/akin/city_card/buscard/core/request/QrScanRequest.java
+++ src/main/java/akin/city_card/buscard/core/request/QrScanRequest.java
@@ -1,14 +1,10 @@
 package akin.city_card.buscard.core.request;
 
 
+import lombok.Data;
+
+@Data
 public class QrScanRequest {
     private String qrToken;
 
-    public String getQrToken() {
-        return qrToken;
-    }
-
-    public void setQrToken(String qrToken) {
-        this.qrToken = qrToken;
-    }
 }
